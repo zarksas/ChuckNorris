e15ae2fa1457b06f09c8f037ff4f8cf76a60d409
username <nomadicalli@mail.ru>
2022-02-13 14:45:38 +0300

    и еще раз

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const div = document.querySelector(".joke");
 const button = document.querySelector("button");
+const showJoke = document.createElement("p");
 
 button.addEventListener("click", () => {
-  const showJoke = document.createElement("p");
   const f = async () => {
     const res = await fetch("https://api.chucknorris.io/jokes/random");
     const joke = await res.json();
     showJoke.textContent = joke.value;
+    div.append(showJoke);
   };
   f();
-
-  div.append(showJoke);
 });
